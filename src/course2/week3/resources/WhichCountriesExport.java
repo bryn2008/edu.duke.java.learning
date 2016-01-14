d3907bb502a2e063148cfed79879a1cf321a673c
Bryn Lloyd <bryn.lloyd@metoffice.gov.uk>
2016-01-14 16:06:36 +0000

    adding resources for course 3 week 2 lessons

---
+++ src/course2/week3/resources/WhichCountriesExport.java
@@ -0,0 +1,37 @@
+package course2.week3.resources;
+
+/**
+ * Reads a chosen CSV file of country exports and prints each country that exports coffee.
+ * 
+ * @author Duke Software Team 
+ */
+
+import edu.duke.*;
+import org.apache.commons.csv.*;
+
+public class WhichCountriesExport {
+	
+	public static void main (String [] args){
+	
+		WhichCountriesExport myObj = new WhichCountriesExport();
+		myObj.whoExportsCoffee();
+
+	}
+	
+	public void listExporters(CSVParser parser, String exportOfInterest) {
+		//for each row in the CSV File
+
+			//Look at the "Exports" column
+
+			//Check if it contains exportOfInterest
+
+				//If so, write down the "Country" from that row
+
+	}
+
+	public void whoExportsCoffee() {
+		FileResource fr = new FileResource();
+		CSVParser parser = fr.getCSVParser();
+		listExporters(parser, "coffee");
+	}
+}
