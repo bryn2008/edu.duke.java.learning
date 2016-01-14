4fbed3b6fffd4d6332cda764be8c2adbb46fd6b3
Bryn Lloyd <bryn.lloyd@metoffice.gov.uk>
2016-01-14 15:29:41 +0000

    started new assignment

---
+++ src/course2/week3/ParsingExportData.java
@@ -0,0 +1,16 @@
+package course2.week3;
+
+public class ParsingExportData {
+
+
+	public static void main (String [] args){
+		
+		ParsingExportData myObj = new ParsingExportData();
+		myObj.tester();
+	}
+	
+	public void tester(){
+		
+	}
+	
+}
