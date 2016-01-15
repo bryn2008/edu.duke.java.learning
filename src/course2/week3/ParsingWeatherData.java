e3cc38a45129ba544cec6418158618a5162d58fa
Bryn Lloyd <bryn.lloyd@metoffice.gov.uk>
2016-01-15 15:57:23 +0000

    added a method to find the lowest temperature

--- src/course2/week3/ParsingWeatherData.java
+++ src/course2/week3/ParsingWeatherData.java
@@ -14,7 +14,72 @@ public class ParsingWeatherData {
 	public static void main (String [] args){
 		
 		ParsingWeatherData myObj = new ParsingWeatherData();
-		myObj.testHottestInManyDays();
+		//myObj.testHottestInManyDays();
+		//myObj.testColdestInDay();
+		myObj.testFileWithColdestTemperature();
+	}
+	
+/********************************************************************************************************/	
+	
+	public CSVRecord coldestHourInFile(CSVParser parser) {
+		
+		//start with largestSoFar as nothing
+		CSVRecord lowestSoFar = null;
+		//For each row (currentRow) in the CSV File
+		for(CSVRecord currentRow : parser){
+			//If largestSoFar is nothing
+			lowestSoFar = getSmallestOfTwo(currentRow, lowestSoFar);
+		}
+		//The largestSoFar is the answer
+		return lowestSoFar;
+	}
+
+/********************************************************************************************************/
+	
+	public CSVRecord fileWithColdestTemperature () {
+		CSVRecord lowestSoFar = null;
+		DirectoryResource dr = new DirectoryResource();
+		//iterate over files
+		for (File f : dr.selectedFiles()){
+			FileResource fr = new FileResource(f);
+
+			
+			//use an if statement to determine the file with the coldest temp
+			
+			
+			//use method to get smallest in file
+			CSVRecord currentRow = coldestHourInFile(fr.getCSVParser());
+			lowestSoFar = getSmallestOfTwo(currentRow, lowestSoFar);
+			
+			
+		} 
+		//The largest so far is the answer
+		return lowestSoFar;
+	}
+		
+/********************************************************************************************************/
+	
+	public CSVRecord getSmallestOfTwo(CSVRecord currentRow, CSVRecord lowestSoFar) {
+		if(lowestSoFar == null){
+			lowestSoFar = currentRow;
+		}
+		//Otherwise
+		else{
+			double currentTemp = Double.parseDouble(currentRow.get("TemperatureF"));
+			double lowestTemp = Double.parseDouble(lowestSoFar.get("TemperatureF"));
+				//Check if currentRow's temperature > largestSoFar
+				if(currentTemp < lowestTemp){
+					int bugTemp = -9999;
+					if(currentTemp == bugTemp){
+						//this stops the bugTemp of -9999 from being counted 
+					}
+					else{
+						//If so update largestSoFar to currentRow
+						lowestSoFar = currentRow;
+					}
+				}
+		}
+		return lowestSoFar;
 	}
 	
 /********************************************************************************************************/	
@@ -47,6 +112,7 @@ public class ParsingWeatherData {
 		//The largest so far is the answer
 		return largestSoFar;
 	}
+	
 /********************************************************************************************************/
 	
 	public CSVRecord getLargestOfTwo(CSVRecord currentRow, CSVRecord largestSoFar) {
@@ -83,5 +149,22 @@ public class ParsingWeatherData {
 	}
 		
 /********************************************************************************************************/
+	//number 1 in the assignment
+	public void testColdestInDay () {
+		FileResource fr = new FileResource("resources/course2/week3/data/2015/weather-2015-01-01.csv");
+		CSVRecord coldest = coldestHourInFile(fr.getCSVParser());
+		System.out.println("coldest temperature was " + coldest.get("TemperatureF") +
+				   " at " + coldest.get("TimeEST"));
+	}
+		
+/********************************************************************************************************/
 	
+	//number 2 in the assignment
+	public void testFileWithColdestTemperature () {
+		CSVRecord coldest = fileWithColdestTemperature();
+		System.out.println("coldest temperature was " + coldest.get("TemperatureF") +
+				   " at " + coldest.get("DateUTC"));
+	}
+	
+/********************************************************************************************************/
 }
