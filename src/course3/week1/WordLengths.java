bbf0ac5ff29127b5e14816a7d3380e20988d813f
Bryn Lloyd <bryn.lloyd@metoffice.gov.uk>
2016-01-27 16:57:01 +0000

    Word Lengths code started

--- src/course3/week1/WordLengths.java
+++ src/course3/week1/WordLengths.java
@@ -1,6 +1,7 @@
 package course3.week1;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import edu.duke.FileResource;
 
@@ -12,18 +13,57 @@ public class WordLengths {
 		myObj.testCountWordLength();
 	}
 	
-	public void countWordLength (FileResource fr, ArrayList<Integer> al){
+	public void countWordLength (FileResource fr){
+		
+		//TO DO CODE
+		
+		String resource = fr.asString();
+		System.out.println("resource: " + resource);
+		
+		ArrayList<String> counts = new ArrayList<String>();
+		ArrayList<Integer> countsOfWords = new ArrayList<Integer>();
+		HashMap<Integer, String> wordMap = new HashMap<Integer, String>();
+		wordMap.put("foo".hashCode(), "bar");
+		
+		for(String word : resource.split("\\s")) {
+			word = word.replaceAll("[+.^:,]","");
+			counts.add(word);
+			//System.out.println(word);
+		}
+		//System.out.println(counts);
+		System.out.println("The size of the arraylist is: " + counts.size());
+		
+		int cw = 0;
+		for (int i =0; i < counts.size(); i++){
+			String currentWord = counts.get(i);
+			cw = currentWord.length();
+			System.out.println(currentWord.length() + " " + counts.get(i));
+			countsOfWords.add(currentWord.length());
+			wordMap.put(cw, currentWord);
+		}
+		
+		System.out.print(countsOfWords);
+		System.out.println(counts);
+		
+		System.out.println(">>" +wordMap);
+		
+		indexOfMax();
 		
 	}
 	
-	
-	
 	public void testCountWordLength (){
 		
-		FileResource fr = new FileResource();
-		ArrayList<Integer> al = new ArrayList<Integer>();
+		FileResource fr = new FileResource("ProgrammingBreakingCaesarData/smallHamlet.txt");
+		//System.out.println(fr.asString());
+		//CALL THE METHOD
+		countWordLength(fr);
 		
-		countWordLength(fr, al);
+	}
+	
+	public void indexOfMax (){
+		//this method should return 3
+		
+		
 		
 		
 	}
