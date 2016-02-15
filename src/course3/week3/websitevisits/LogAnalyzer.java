96e9d4f1dc68a58aec6ae467a42a92da31e1d867
Bryn Lloyd <bryn.lloyd@metoffice.gov.uk>
2016-02-15 15:13:16 +0000

    Started CountingWebsiteVisits Class

---
+++ src/course3/week3/websitevisits/LogAnalyzer.java
@@ -0,0 +1,92 @@
+package course3.week3.websitevisits;
+
+/**
+ * Write a description of class LogAnalyzer here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+import java.util.*;
+
+import edu.duke.*;
+
+public class LogAnalyzer {
+	private ArrayList<LogEntry> records;
+
+	public LogAnalyzer() {
+
+		records = new ArrayList<LogEntry>();
+
+	}
+
+	public void readFile(String filename) {
+
+		FileResource fr = new FileResource(filename);
+		for (String currentLine : fr.lines()) {
+			LogEntry newLogEntry = WebLogParser.parseEntry(currentLine);
+			records.add(newLogEntry);
+		}
+
+	}
+
+	public int countUniqueIPs() {
+
+		ArrayList<String> UniqueIPs = new ArrayList<String>();
+		for (LogEntry le : records) {
+			String ipAddress = le.getIpAddress();
+			if (!UniqueIPs.contains(ipAddress)) {
+				UniqueIPs.add(ipAddress);
+			}
+		}
+		return UniqueIPs.size();
+	}
+	
+	public void printAllHigherThanNum(int num) {
+		for (LogEntry le : records) {
+			if (le.getStatusCode() > num){
+				System.out.println(le);
+			}
+		}
+	}
+	
+	public ArrayList<String> uniqueIPVisitsOnDay(String someday) {
+		
+		ArrayList<String> UniqueIPs = new ArrayList<String>();
+		ArrayList<String> UniqueIPsOnADay = new ArrayList<String>();
+		for (LogEntry le : records) {
+			String ipAddress = le.getIpAddress();
+			if(le.getAccessTime().toString().substring(4, 10).equals(someday)){
+				if (!UniqueIPs.contains(ipAddress)) {
+					UniqueIPsOnADay.add(ipAddress);
+					UniqueIPs.add(ipAddress);
+				}
+			}
+		}
+		return UniqueIPsOnADay;
+		
+	}
+	
+	public int countUniqueIPsInRange(int low, int high){
+		
+		ArrayList<String> UniqueIPsInRange = new ArrayList<String>();
+		for (LogEntry le : records) {
+			String ipAddress = le.getIpAddress();
+			int statusCode =  le.getStatusCode();
+			if (!UniqueIPsInRange.contains(ipAddress)) {
+				if (statusCode >= low && statusCode <= high){
+					//System.out.println(le);
+					UniqueIPsInRange.add(ipAddress);
+				}
+			}
+		}
+		return UniqueIPsInRange.size();
+	}
+	
+	public void printAll() {
+		for (LogEntry le : records) {
+			System.out.println(le);
+		}
+	}
+
+}
---
+++ src/course3/week3/websitevisits/Tester.java
@@ -0,0 +1,102 @@
+package course3.week3.websitevisits;
+
+/**
+ * Write a description of class Tester here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+import java.util.*;
+
+public class Tester {
+	
+	public static void main(String[] args){
+		
+		Tester myObj = new Tester(); 
+		//myObj.testLogAnalyzer();
+		//myObj.testUniqueIPs();
+		myObj.testPrintAllHigherThanNum();
+		myObj.testUniqueIPVisitsOnDay();
+		myObj.testCountUniqueIPsInRange();
+	}
+	
+    public void testLogEntry() {
+        LogEntry le = new LogEntry("1.2.3.4", new Date(), "example request", 200, 500);
+        System.out.println(le);
+        LogEntry le2 = new LogEntry("1.2.100.4", new Date(), "example request 2", 300, 400);
+        System.out.println(le2);
+    }
+    
+    public void testLogAnalyzer() {
+    	
+    	testLogEntry();
+    	
+    	LogAnalyzer la = new LogAnalyzer();
+    	String filename = "ProgrammingWebServerLogs/short-test_log";
+    	la.readFile(filename);
+    	la.printAll();
+    }
+    
+    public void testUniqueIPs() {
+    	
+    	LogAnalyzer la = new LogAnalyzer();
+    	String filename = "ProgrammingWebServerLogs/short-test_log";
+    	la.readFile(filename);
+    	int uniqueIPs = la.countUniqueIPs();
+    	System.out.println("There are "+uniqueIPs+" unique IPs");
+    }
+    
+    public void testPrintAllHigherThanNum() {
+    	
+    	LogAnalyzer la = new LogAnalyzer();
+    	String filename = "ProgrammingWebServerLogs/weblog1_log";
+    	la.readFile(filename);
+    	/*int statusCode = 300;
+    	System.out.println("All addresses with a status code higher that \""+statusCode+"\" are:");
+    	la.printAllHigherThanNum(statusCode);*/
+    	
+    	int statusCode = 400;
+    	System.out.println("All addresses with a status code higher that \""+statusCode+"\" are:");
+    	la.printAllHigherThanNum(statusCode);
+    }
+    
+    public void testUniqueIPVisitsOnDay() {
+    	
+    	LogAnalyzer la = new LogAnalyzer();
+    	String filename = "ProgrammingWebServerLogs/weblog1_log";
+    	la.readFile(filename);
+    	//la.printAll();
+    	//String dayOne = "Sep 14";
+    	//String dayTwo = "Sep 30";
+    	//ArrayList<String> al1 = la.uniqueIPVisitsOnDay(dayOne);
+    	//ArrayList<String> al2 = la.uniqueIPVisitsOnDay(dayTwo);
+    	//System.out.println("ArrayList 1, two items = "+al1);
+    	//System.out.println("Arraylist 2, three items = "+al2);
+    	//System.out.println("The number of unique IP visits for "+ dayOne +" is "+al1.size()+".");
+    	//System.out.println("The number of unique IP visits for "+ dayTwo +" is "+al2.size()+".");
+    	
+    	String dayOne = "Mar 24";
+    	ArrayList<String> al1 = la.uniqueIPVisitsOnDay(dayOne);
+    	System.out.println("The number of unique IP visits for "+ dayOne +" is "+al1.size()+".");
+    }
+    
+    public void testCountUniqueIPsInRange() {
+    	
+    	LogAnalyzer la = new LogAnalyzer();
+    	String filename = "ProgrammingWebServerLogs/weblog1_log";
+    	la.readFile(filename);
+    	//la.printAll();
+    	
+    	int low = 200; 
+    	int high = 299;
+    	int total = la.countUniqueIPsInRange(low, high);
+    	System.out.println("The total number of unique IP Addresses with a status code between " + low + " and " + high + " is " + total +"\n");
+    	
+    	/*int lowTwo = 300; 
+    	int highTwo = 399;
+    	int totalTwo = la.countUniqueIPsInRange(lowTwo, highTwo);
+    	System.out.println("The total number of unique IP Addresses with a status code between " + lowTwo + " and " + highTwo + " is " + totalTwo);*/
+    } 
+
+}
