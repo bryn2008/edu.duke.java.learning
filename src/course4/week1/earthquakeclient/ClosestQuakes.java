e0cbdde6958f795f4d1ae0cf45ac802b69bdc808
Bryn Lloyd <bryn.lloyd@metoffice.gov.uk>
2016-02-23 15:29:31 +0000

    updated class to complete assignment 4

--- src/course4/week1/earthquakeclient/ClosestQuakes.java
+++ src/course4/week1/earthquakeclient/ClosestQuakes.java
@@ -47,14 +47,14 @@ public class ClosestQuakes {
 
 	public void findClosestQuakes() {
 		EarthQuakeParser parser = new EarthQuakeParser();
-		String source = filePath + "data/nov20quakedata.atom";
+		String source = filePath + "data/nov20quakedatasmall.atom";
 		//String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
 		ArrayList<QuakeEntry> list = parser.read(source);
 		System.out.println("read data for " + list.size());
 
 		Location jakarta = new Location(-6.211, 106.845);
-
-		ArrayList<QuakeEntry> close = getClosest(list, jakarta, 10);
+		
+		ArrayList<QuakeEntry> close = getClosest(list, jakarta, 3);
 		for (int k = 0; k < close.size(); k++) {
 			QuakeEntry entry = close.get(k);
 			double distanceInMeters = jakarta.distanceTo(entry.getLocation());
