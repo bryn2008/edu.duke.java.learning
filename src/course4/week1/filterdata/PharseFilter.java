005f152529ac458c24365fe9b1991ef7abdf5494
Bryn Lloyd <bryn.lloyd@metoffice.gov.uk>
2016-02-25 15:41:25 +0000

    created phrase filter class

---
+++ src/course4/week1/filterdata/PharseFilter.java
@@ -0,0 +1,25 @@
+package course4.week1.filterdata;
+
+public class PharseFilter implements Filter {
+	
+	private String where;
+	private String phrase;
+	
+	public PharseFilter(String place, String str) {
+		where = place;
+		phrase = str;
+	}
+	
+	@Override
+	public boolean satisfies(QuakeEntry qe) {
+		if("start".equalsIgnoreCase(where)){
+			return qe.getInfo().substring(0, phrase.length()).equals(phrase);
+		}else if("end".equalsIgnoreCase(where)){
+			return qe.getInfo().substring(qe.getInfo().length() - phrase.length()).equals(phrase);
+		}else if("any".equalsIgnoreCase(where)){
+			return qe.getInfo().contains(phrase);
+		}
+		return false;
+	}
+	
+}
