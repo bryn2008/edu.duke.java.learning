97fd920dbef26246b593bb18a3d6e49652df8a04
Bryn Lloyd <bryn.lloyd@metoffice.gov.uk>
2016-02-26 11:17:19 +0000

    week 3 resources added

---
+++ src/course4/week2/resources/quakesortdemo/QuakeSortDemo.java
@@ -0,0 +1,25 @@
+package course4.week2.resources.quakesortdemo;
+
+/**
+ * Illustrate sorting
+ * @author Duke Software 
+ * @version 1.0
+ */
+import edu.duke.*;
+import java.util.*;
+
+public class QuakeSortDemo {
+    
+    public void testSort(){
+        EarthQuakeParser parser = new EarthQuakeParser();
+        String source = "data/nov20quakedata.atom";
+        //String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
+        
+        ArrayList<QuakeEntry> list  = parser.read(source);
+        Collections.sort(list);
+        
+        for(QuakeEntry qe: list) {
+            System.out.println(qe);
+        }
+    }
+}
